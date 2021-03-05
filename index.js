616153c6cce8f24eee7e7f9dd9c386127b6c824f
viny-147 <vinayn0147@gmail.com>
2021-03-05 17:05:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+if(randomNumber1==1){
+    document.querySelector(".img1").setAttribute("src","images/dice1.png");
+}
+else if(randomNumber1==2){
+    document.querySelector(".img1").setAttribute("src","images/dice2.png");
+}
+else if(randomNumber1==3){
+    document.querySelector(".img1").setAttribute("src","images/dice3.png");
+}
+else if(randomNumber1==4){
+    document.querySelector(".img1").setAttribute("src","images/dice4.png");
+}
+else if(randomNumber1==5){
+    document.querySelector(".img1").setAttribute("src","images/dice5.png");
+}
+else{
+    document.querySelector(".img1").setAttribute("src","images/dice6.png");
+}
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+if(randomNumber2==1){
+    document.querySelector(".img2").setAttribute("src","images/dice1.png");
+}
+else if(randomNumber2==2){
+    document.querySelector(".img2").setAttribute("src","images/dice2.png");
+}
+else if(randomNumber2==3){
+    document.querySelector(".img2").setAttribute("src","images/dice3.png");
+}
+else if(randomNumber2==4){
+    document.querySelector(".img2").setAttribute("src","images/dice4.png");
+}
+else if(randomNumber2==5){
+    document.querySelector(".img2").setAttribute("src","images/dice5.png");
+}
+else{
+    document.querySelector(".img2").setAttribute("src","images/dice6.png");
+}
+
+
+if(randomNumber1<randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player 2 won <img class='flag-img' src='images/flag.png'>";
+}
+else if(randomNumber1==randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Draw!..";
+}
+else
+{
+    document.querySelector("h1").innerHTML="<img class='flag-img' src='images/flag.png'> Player 1 won";
+}
